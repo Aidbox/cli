c4a2e288711b8e1c974c64acf10011eb2dcabdb4
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2023-03-30 16:16:35 +0200

    init aidbox cli

---
+++ src/aidbox_cli.clj
@@ -0,0 +1,11 @@
+(ns aidbox-cli
+  (:gen-class)
+  (:require [zen.cli]
+            [zen.core]
+            [generator.cli]))
+
+(defn -main [& args]
+  (let [ztx (zen.core/new-context)
+        _ (zen.core/read-ns ztx 'aidbox-cli)]
+      (zen.cli/cli ztx 'aidbox-cli/config args)
+      (System/exit 0)))
